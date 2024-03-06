b8f97b212a4a4af8d5a39690a7ca0543a0f3feb7
Blossom <146731612+BlossomAnukposi@users.noreply.github.com>
2024-03-06 13:13:18 +0100

    Updated MOVE_FORWARD file and uploaded tests

---
+++ Sir RAMAV/MOVE FORWARD/tests.ino
@@ -0,0 +1,23 @@
+//  // ULTRASONIC TEST: Checking distance with the serial monitor
+//  Serial.print("Distance: ");
+//  Serial.println(distance);
+//  delay(100);
+
+  //ANALOG LINE TEST: Checking the line sensors that are activated
+//  Serial.print("S1: ");
+//  Serial.println(sensor1);
+//  Serial.print("S2: ");
+//  Serial.println(sensor2);
+//  Serial.print("S3: ");
+//  Serial.println(sensor3);
+//  Serial.print("S4: ");
+//  Serial.println(sensor4);
+//  Serial.print("S5: ");
+//  Serial.println(sensor5);
+//  Serial.print("S6: ");
+//  Serial.println(sensor6);
+//  Serial.print("S7: ");
+//  Serial.println(sensor7);
+//  Serial.print("S8: ");
+//  Serial.println(sensor8);
+//  delay(1000);
