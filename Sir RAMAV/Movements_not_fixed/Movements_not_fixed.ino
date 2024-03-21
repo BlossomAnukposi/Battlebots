3e995d340de749227c8a0829f4571ac48d5328ee
BlossomAnukposi <blossom.anukposi1@student.nhlstenden.com>
2024-03-21 22:53:28 +0100

    Fixed movement turn logic and set up rotation sensors

--- Sir RAMAV/Movements_not_fixed/Movements_not_fixed.ino
+++ Sir RAMAV/Movements_not_fixed/Movements_not_fixed.ino
@@ -43,8 +43,30 @@ void signalTurnAround()
     strip_NI.show();
 }
 
+void signalLeft()
+{
+    strip_NI.begin();
+    for (int j = 0; j < NEO_PIXNUMBER; j++)
+    {
+        strip_NI.setPixelColor(j, strip_NI.Color(GREEN));
+    }
+    strip_NI.setPixelColor(2, strip_NI.Color(BLUE));
+    strip_NI.show();
+}
+
+void signalRight()
+{
+    strip_NI.begin();
+    for (int j = 0; j < NEO_PIXNUMBER; j++)
+    {
+        strip_NI.setPixelColor(j, strip_NI.Color(GREEN));
+    }
+    strip_NI.setPixelColor(3, strip_NI.Color(BLUE));
+    strip_NI.show();
+}
+
 //=============== ANALOG LINE SENSORSS ===============//
-#define AVERAGE                    700
+#define AVERAGE                    600
 #define SENSORS                    8
 int     S[SENSORS] =               {A0, A1, A2, A3, A4, A5, A6, A7};
 int     IR[SENSORS];
@@ -61,12 +83,39 @@ void readSensors()
 //===================== CALIBRATION =====================//
 
 
-//===================== MOTORS =====================//
-#define MOTOR_LEFT_FORWARD    3 // connected to B2
-#define MOTOR_RIGHT_FORWARD   5 // connected to A1
-#define MOTOR_LEFT_BACKWARD   9 // connected to B1
-#define MOTOR_RIGHT_BACKWARD  6 // connected to A2
+//===================== ROTATION SENSORS =====================//
+#define ROTOR_RIGHT           2
+#define ROTOR_LEFT            3
 
+int readPosition = 0;
+int lastRead = 0;
+long lastReadTime = 0;
+
+void rotorSensor() {
+    int right =   digitalRead(ROTOR_RIGHT);
+    int left =    digitalRead(ROTOR_LEFT);
+    int reading = (right << 1) | left;
+    int sum  =    (lastRead << 2) | reading;
+    
+    if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) 
+    {
+        readPosition++;
+    } 
+    
+    else if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) 
+    {
+        readPosition--;
+    }
+    
+    lastRead = reading;
+    lastReadTime = millis();
+}
+
+//===================== MOTORS =====================//
+#define MOTOR_LEFT_FORWARD    5 // connected to B2
+#define MOTOR_RIGHT_FORWARD   10 // connected to A1
+#define MOTOR_LEFT_BACKWARD   9 // connected to B1
+#define MOTOR_RIGHT_BACKWARD  11 // connected to A2
 
 void slightLeft()
 {
@@ -86,7 +135,7 @@ void slightRight()
 
 void turnRight()
 {
-    analogWrite(MOTOR_LEFT_FORWARD, 255);
+    analogWrite(MOTOR_LEFT_FORWARD, 220);
     analogWrite(MOTOR_RIGHT_FORWARD, 0);
     analogWrite(MOTOR_LEFT_BACKWARD, 0);
     analogWrite(MOTOR_RIGHT_BACKWARD, 0);
@@ -95,7 +144,7 @@ void turnRight()
 void turnLeft()
 {
     analogWrite(MOTOR_LEFT_FORWARD, 0);
-    analogWrite(MOTOR_RIGHT_FORWARD, 255);
+    analogWrite(MOTOR_RIGHT_FORWARD, 210);
     analogWrite(MOTOR_LEFT_BACKWARD, 0);
     analogWrite(MOTOR_RIGHT_BACKWARD, 0);
 }
@@ -119,12 +168,12 @@ void stopMotors()
 
 void adjustPath()
 {   
-    if (IR[3] > AVERAGE && IR[4] > AVERAGE && IR[6] < AVERAGE)
+    if (IR[2] > AVERAGE && IR[3] > AVERAGE && IR[5] < AVERAGE)
     {
         slightRight();
     }
 
-    else if (IR[3] < AVERAGE && IR[5] > AVERAGE && IR[6] > AVERAGE)
+    else if (IR[2] < AVERAGE && IR[4] > AVERAGE && IR[5] > AVERAGE)
     {
         slightLeft();
     }
@@ -146,42 +195,71 @@ void setup()
     pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
     pinMode(MOTOR_LEFT_BACKWARD, OUTPUT);
     pinMode(MOTOR_RIGHT_BACKWARD, OUTPUT);
+    pinMode(ROTOR_LEFT, INPUT);
+    pinMode(ROTOR_RIGHT, INPUT);
 
     for (int i = 0; i < SENSORS; i++)
     {
         pinMode(IR[i], INPUT);
     }
 
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A), updateEncoder, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_B), updateEncoder, CHANGE);
+
     Serial.begin(9600);
 }
 
 void loop() 
 {
-    readSensors();
+      moveForward();
+//    readSensors();
 
-    if (IR[1] > AVERAGE && IR[2] > AVERAGE && IR[4] > AVERAGE && IR[7] < AVERAGE && IR[8] < AVERAGE)
+    if (IR[0] > AVERAGE && IR[1] > AVERAGE && IR[3] > AVERAGE && IR[6] < AVERAGE && IR[7] < AVERAGE)
     {
-        delay(100);
         turnRight();
+        signalLeft();
+//        delay(700);
     }
-    
-    else if (IR[1] < AVERAGE && IR[2] < AVERAGE && IR[5] > AVERAGE && IR[7] > AVERAGE && IR[8] > AVERAGE)
+
+    readSensors();
+    while (IR[0] < AVERAGE && IR[1] < AVERAGE && IR[4] > AVERAGE && IR[6] > AVERAGE && IR[7] > AVERAGE)
     {
-        delay(100);
         turnLeft();
+        signalRight();
+//        delay(700);
     }
 
-    else if (IR[1] < AVERAGE && IR[2] < AVERAGE && IR[3] < AVERAGE && IR[4] < AVERAGE && IR[5] < AVERAGE && IR[6] < AVERAGE && IR[7] < AVERAGE && IR[8] < AVERAGE)
+    readSensors();
+    if (IR[0] < AVERAGE && IR[1] < AVERAGE && IR[2] < AVERAGE && IR[3] < AVERAGE && IR[4] < AVERAGE && IR[5] < AVERAGE && IR[6] < AVERAGE && IR[7] < AVERAGE)
     {
-        delay(100);
         turnAround();
+        signalTurnAround();
+//        delay(1000);
     }
 
-    else
+    readSensors();
+    if (IR[0] > AVERAGE && IR[1] > AVERAGE && IR[3] > AVERAGE && IR[4] > AVERAGE && IR[6] > AVERAGE && IR[7] > AVERAGE)
     {
-        adjustPath();
+        turnRight();
+        signalLeft();
+
+        if (IR[0] < AVERAGE && IR[1] < AVERAGE && IR[3] > AVERAGE && IR[4] > AVERAGE && IR[6] < AVERAGE && IR[7] < AVERAGE)
+        {
+            moveForward();
+            signalForward();
+        }
     }
 
-    moveForward(); //------ Change (A): Removed move forward from the loop to make sure the turns execute by all means ------//
-    signalOff();
+    readSensors();
+    if (IR[0] < AVERAGE && IR[1] < AVERAGE && IR[3] > AVERAGE && IR[4] > AVERAGE && IR[6] < AVERAGE && IR[7] < AVERAGE)
+    {
+        moveForward();
+        signalForward();
+    }
+
+    Serial.println(IR[0]);
+    Serial.println(IR[1]);
+    Serial.println(IR[6]);
+    Serial.println(IR[7]);
+
 }
