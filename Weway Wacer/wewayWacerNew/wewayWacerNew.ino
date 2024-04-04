c2d8163079746ab8ec9c7ad28392e7681f1c5d9c
QuentinHamelet <quentin.hamelet-rascol@student.nhlstenden.com>
2024-04-04 23:54:37 +0200

    Fixed gripper values, added neopixle functions

--- Weway Wacer/wewayWacerNew/wewayWacerNew.ino
+++ Weway Wacer/wewayWacerNew/wewayWacerNew.ino
@@ -17,7 +17,7 @@
 //====[ PIN NUMBERS ]=====================
 
 //Neopixles
-#define NEOPIN_INPUT    13
+#define NEOPIN 13
 
 //Servomotor: Gripper
 #define gripper 7
@@ -41,21 +41,21 @@
 //====[ VARIABLES ]=======================
 
 //Neopixles
-#define WHITE            255, 255, 255
-#define RED             0  , 255, 0
-#define GREEN           255, 0  , 0
-#define BLUE            0  , 0  , 255
+#define WHITE           255, 255, 255
+#define RED             0, 255, 0
+#define GREEN           255, 0, 0
+#define BLUE            0, 0, 255
 #define LIGHT_GREEN     255, 155, 0
 #define YELLOW          255, 255, 0
-#define OFF             0  , 0  , 0
+#define CALIBRATE       0, 200, 190
+#define OFF             0, 0, 0
 #define NEO_PIXNUMBER   4                       // number of neopixle LEDs
-Adafruit_NeoPixel neoPixel(NEO_PIXNUMBER, NEOPIN_INPUT, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel neoPixel(NEO_PIXNUMBER, NEOPIN, NEO_GRB + NEO_KHZ800);
 
 //Servomotor: Gripper
-#define gripperOpenPulse 1000
-#define gripperClosePulse 500
+#define gripperOpenPulse 1300
+#define gripperClosePulse 1000
 #define gripperPulseRepeat 20
-#define gripperToggleThing 1000
 
 //Ultrasonic sensors
 #define SPEED_OF_SOUND 0.034                      // in cm/s
@@ -109,7 +109,8 @@ bool onSquareStart = false; // Flag to track whether the robot is on the square
 
 //====[ SETUP ]===========================
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
   
   //Electromotors
@@ -153,7 +154,8 @@ void setupIRSensors(){
  pinMode(A7, INPUT);
 }
 
-void readSensors(){
+void readSensors()
+{
   IR1 = analogRead(A0);
   IR2 = analogRead(A1);
   IR3 = analogRead(A2);
@@ -166,12 +168,13 @@ void readSensors(){
 
 //====[ LOOP ]============================
 
-void loop() {
+void loop() 
+{
   distanceFront = measureDistance(0);
   distanceRight = measureDistance(1);
   averageFront = averageDistanceFront();
   averageRight = averageDistanceRight();
-  
+
 //  // If there's a wall in front of the robot
 //  if (averageFront < 25) {
 //      // If there's also a wall on the right, turn left
@@ -196,32 +199,40 @@ void loop() {
 //      }
 //    }
 
+//gripper value test
   for (int i = 0; i < 8; i++)
   {
-     setGripper(gripperClose Pulse);
+     setGripper(gripperOpenPulse);
+     signalCalibrate();
   }
 }
 
 //====[ FUNCTIONS ]=======================
 
 //Rotation sensor
-void countLeftSensor() {
+void countLeftSensor() 
+{
   leftRotationCount++;
 }
 
-void countRightSensor() {
+void countRightSensor() 
+{
   rightRotationCount++;
 }
 
 //Ultrasonic sensor
-long measureDistance(int sensor) {
+long measureDistance(int sensor) 
+{
     int triggerPin, echoPin;
     long* measurements;
-    if (sensor == 0) { // 0 for front sensor
+    if (sensor == 0) // 0 for front sensor
+    { 
         triggerPin = frontTrigger;
         echoPin = frontEcho;
         measurements = measurementsFront;
-    } else { // 1 for right sensor
+    } 
+    else // 1 for right sensor
+    { 
         triggerPin = rightTrigger;
         echoPin = rightEcho;
         measurements = measurementsRight;
@@ -239,7 +250,8 @@ long measureDistance(int sensor) {
     long distance = (duration * SPEED_OF_SOUND) / 2;
 
     // Shift the old measurements and add the new one.
-    for (int i = 0; i < NUM_MEASUREMENTS - 1; i++) {
+    for (int i = 0; i < NUM_MEASUREMENTS - 1; i++) 
+    {
         measurements[i] = measurements[i + 1];
     }
     measurements[NUM_MEASUREMENTS - 1] = distance;
@@ -247,24 +259,29 @@ long measureDistance(int sensor) {
     return distance;
 }
 
-long averageDistanceFront() {
+long averageDistanceFront() 
+{
     long sum = 0;
-    for (int i = 0; i < NUM_MEASUREMENTS; i++) {
+    for (int i = 0; i < NUM_MEASUREMENTS; i++) 
+    {
         sum += measurementsFront[i];
     }
     return round(static_cast<float>(sum) / NUM_MEASUREMENTS);
 }
 
-long averageDistanceRight() {
+long averageDistanceRight() 
+{
     long sum = 0;
-    for (int i = 0; i < NUM_MEASUREMENTS; i++) {
+    for (int i = 0; i < NUM_MEASUREMENTS; i++) 
+    {
         sum += measurementsRight[i];
     }
     return round(static_cast<float>(sum) / NUM_MEASUREMENTS);
 }
 
 //Electromotors
-void stopMotors(){
+void stopMotors()
+{
     analogWrite(motorLeftForward,0);
     analogWrite(motorRightForward,0);
     analogWrite(motorLeftBackward,0);
@@ -286,7 +303,7 @@ void forwardCM(int distance)
     unsigned long startTime = millis();
     unsigned long timeout = distance * 10; 
 
-    while((rightRotationCount < distance*2) && (leftRotationCount < distance*2))
+    while ((rightRotationCount < distance*2) && (leftRotationCount < distance*2))
     {
         if (millis() - startTime > timeout) {
             Serial.println("Timeout reached, stopping motors");
@@ -318,7 +335,7 @@ void right()
     analogWrite(motorRightForward, 204);
 
     // Keep turning until timeout
-    while(millis() - startTime < timeout);
+    while (millis() - startTime < timeout);
 
     // Stop the motors
     stopMotors();
@@ -331,13 +348,15 @@ void right()
     robotState = Moving;
 }
 
-void wait(unsigned long duration) {
+void wait(unsigned long duration) 
+{
     stopMotors(); // Call the function to stop the motors
     unsigned long startTime = millis();
-    while(millis() - startTime < duration);
+    while (millis() - startTime < duration);
     robotState = Stopped;
 }
 
+//Should have a fix right?
 void fixLeft()
 {
     analogWrite(motorLeftForward, 235);
@@ -371,11 +390,16 @@ void left()
     robotState = Moving;
 }
 
-bool isStuck() {
-    return (leftRotationCount == prevLeftRotationCount || rightRotationCount == prevRightRotationCount) && robotState == Moving && (millis() - moveStartTime > 1000);
+bool isStuck() 
+{
+    return (leftRotationCount == prevLeftRotationCount 
+    || rightRotationCount == prevRightRotationCount) 
+    && robotState == Moving 
+    && (millis() - moveStartTime > 1000);
 }
 
-void backup() {
+void backup() 
+{
     wait(100);
     backwards();
     delay(300);
@@ -383,34 +407,29 @@ void backup() {
     robotState = BackingUp;
 }
 
-void avoidStuck() {
-    if (isStuck()) {
+void avoidStuck() 
+{
+    if (isStuck()) 
+    {
         backup();
     }
     prevLeftRotationCount = leftRotationCount;
     prevRightRotationCount = rightRotationCount;
 }
 
-//Neopixles
-void lightsOff() {
-    for (int i = 0; i < NEO_PIXNUMBER; i++) {
-        neoPixel.setPixelColor(i,neoPixel.Color(OFF));
-        neoPixel.show();
-    }
-}
-
-
 //Gripper
-void setServoAngle(int angle) {
+void setServoAngle(int angle) 
+{
     int pulseWidth = map(angle, 0, 180, 0, 255); 
     analogWrite(gripper, pulseWidth);
 }
 
 //GRIPPER TEST FUNCTIONS
-void setGripper(int pulse) {
+void setGripper(int pulse) 
+{
   static unsigned long timer;
   static int pulse1;
-  if(pulse > 0)
+  if (pulse > 0)
   {
     pulse1 = pulse;
   }
@@ -431,37 +450,164 @@ void start()
         if (!onSquareStart) { // If the robot just entered the square
           onSquareStart = true; // Set the flag to true
           squareStartTime = millis(); // Record the start time
-    } else if (millis() - squareStartTime > 200) { // If the robot has been on the square for more than 2 seconds
+    } 
+    else if (millis() - squareStartTime > 200) // If the robot has been on the square for more than 2 seconds
+    { 
       stopMotors();
-      setServoAngle(85);
+      for (int i = 0; i < 8; i++)
+      {
+        setGripper(gripperClosePulse);
+      }
       wait(800); // Wait for 1 second
       // Make a left turn
       left();
       pickedUpObject = true; // Set the flag to indicate object picked up
     }
-  } else {
+  } 
+  else 
+  {
     // If the robot is not on the square, reset the flag
     onSquareStart = false;
-         }
+  }
 }
 
 void endRace()
 {
   readSensors();
-  if (IR1 > 800 && IR2 > 800 && IR3 > 800 && IR6 > 800 && IR7 > 800 && IR8 > 800) {
-    if (!onSquare) { // If the robot just entered the square
+  if (IR1 > 800 && IR2 > 800 && IR3 > 800 && IR6 > 800 && IR7 > 800 && IR8 > 800) 
+  {
+    if (!onSquare) // If the robot just entered the square
+    { 
       onSquare = true; // Set the flag to true
       squareEndTime = millis(); // Record the start time
-    } else if (millis() - squareEndTime > 75) { // If the robot has been on the square for more than 2 seconds
+    } 
+    else if (millis() - squareEndTime > 75) // If the robot has been on the square for more than 2 seconds
+    { 
       stopMotors();
       // Drop the object
-      setServoAngle(130); // Assuming this is how you open the grip to drop the object
+      for (int i = 0; i < 8; i++) // Assuming this is how you open the grip to drop the object
+      {
+        setGripper(gripperOpenPulse);
+      } 
       wait(300); // Wait for 1 second to ensure the grip opens fully
       backup(); // Move backward after dropping the object
       raceFinished = true; // Set the raceFinished flag to true
     }
-  } else {
+  } 
+  else 
+  {
     // If the robot is not on the square, reset the flag
     onSquare = false;
-         }
+  }
+}
+
+//Neopixles
+void lightsOff() 
+{
+    for (int i = 0; i < NEO_PIXNUMBER; i++) 
+    {
+        neoPixel.setPixelColor(i,neoPixel.Color(OFF));
+    }
+    neoPixel.show();
+}
+
+void signalStarting()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(GREEN)); 
+  }
+  neoPixel.show();
+  delay(150);
+  for (int i = 0; i < NEO_PIXNUMBER; i++) 
+  {
+      neoPixel.setPixelColor(i,neoPixel.Color(OFF));
+  }
+  neoPixel.show();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(GREEN)); 
+  }
+  neoPixel.show();
+  delay(150);
+  for (int i = 0; i < NEO_PIXNUMBER; i++) 
+  {
+      neoPixel.setPixelColor(i,neoPixel.Color(OFF));
+  }
+  neoPixel.show();
+}
+
+void signalForward()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(GREEN));
+  }
+  neoPixel.show();
+}
+
+void signalReverse()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(YELLOW));
+  }
+  neoPixel.show();
+}
+
+void signalWait()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(WHITE));
+  }
+  neoPixel.show();
+}
+
+void signalCalibrate()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(CALIBRATE));
+  }
+  neoPixel.show();
+}
+
+void signalStop()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(RED));
+  }
+  neoPixel.show();
+}
+
+void signalLeft()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(GREEN));
+  }
+  neoPixel.setPixelColor(2, neoPixel.Color(BLUE));
+  neoPixel.setPixelColor(1, neoPixel.Color(BLUE));
+  neoPixel.show();
+}
+
+void signalRight()
+{
+  neoPixel.begin();
+  for (int i = 0; i < NEO_PIXNUMBER; i++)
+  {
+    neoPixel.setPixelColor(i, neoPixel.Color(GREEN));
+  }
+  neoPixel.setPixelColor(3, neoPixel.Color(BLUE));
+  neoPixel.setPixelColor(0, neoPixel.Color(BLUE));
+  neoPixel.show();
 }
